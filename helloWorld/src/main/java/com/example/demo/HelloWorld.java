6b151d218a170f074ff503c7fa80f350067c486b
Ankit Mansinghani <ankitmansinghani21@gmail.com>
2019-06-04 21:58:51 +0530

    First commit from git bash

---
+++ helloWorld/src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorld {
+
+	@Bean(name = "ChamanJinga")
+	protected HelloWorld helloWorld() {
+		System.out.println("Hellow World");
+		return new HelloWorld();
+		
+	}
+}
